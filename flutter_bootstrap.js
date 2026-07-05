b976856c5dda015074568764894b046fe665bf4e
lifestyleFraB <werbeagentur.marbe@t-online.de>
2026-07-05 08:16:47 +0200

    Built web
    Branch: main
    Commit: 6a535f6ff76c81ad62c8c5858555b48d92eb507d
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "548504696"
+    serviceWorkerVersion: "741320405"
   }
 });
